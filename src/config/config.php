7433236f7c6be5785bcc21fdda9751d5b9ab13a4
Austin Kregel <austin@kbcomp.co>
2016-07-11 15:41:26 -0400

    Update conig

--- src/config/config.php
+++ src/config/config.php
@@ -2,5 +2,7 @@
 
 return [
     'local-routes' => false,
-    
+    'middleware-api' => ['api', 'jwt.auth'],
+    'middleware' => ['web', 'auth'],
+    'base-layout' => 'vendor.spark.layouts.app'
 ];
